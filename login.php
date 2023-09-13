8ce9c522900cca777435a5f02ee6d3eb01e77ff5
Karen Olmedo <al221411711@gmail.com>
2023-09-13 11:08:16 -0600

    primer commit

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>logines </title>
+</head>
+
+     <body>
+
+  <!------------------------------------------------------->   
+  <!----------------EMPIEZA LA TABLA----------------------->
+<table boder="2">
+<tr>
+<td>ID</td>
+<td>USER</td>
+<td>NAME</td>
+<td>STATUS</td>
+</tr>
+   
+<tr>
+<td>1</td>
+<td>danini</td>
+<td>Daniel</td>
+<td>Activo</td>
+</tr>
+
+<tr>
+<td>2</td>
+<td>Rober</td>
+<td>Roberto</td>
+<td>Activo</td>
+</tr>
+</table>
+<!------------------------------------------------------->   
+  <!----------------TERMINA LA TABLA--------------------->
+  
+</body>
+
