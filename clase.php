b9a53eae7155914869cece51b1c87816441a73e0
Karen Olmedo <al221411711@gmail.com>
2023-09-13 12:35:32 -0600

    agrege la clase

---
+++ clase.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>logines </title>
+</head>
+
+
+<!---ESTE ES UN COMENTARIO------------->
+     <body>
+
+  <!------------------------------------------------------->   
+  <!----------------EMPIEZA LA TABLA----------------------->
+  <!----------------EMPIEZA LA TABLA----------------------->
+<table boder="2">
+<tr>
+<td>ID</td>
+<td>USER</td>
+<td>NAME</td>
+<td>STATUS</td>
+</tr>
+   
+<tr>
+<td>1</td>
+<td>danini</td>
+<td>Daniel</td>
+<td>Activo</td>
+</tr>
+
+<tr>
+<td>2</td>
+<td>Rober</td>
+<td>Roberto</td>
+<td>Activo</td>
+</tr>
+<tr>
+<td>3</td>
+<td>Manene</td>
+<td>Marlene</td>
+<td>Activo</td>
+</tr>
+<tr>
+<td>4</td>
+<td>crjs</td>
+<td>Cesar</td>
+<td>Activo</td>
+</tr>
+</table>
+<!------------------------------------------------------->   
+  <!----------------TERMINA LA TABLA--------------------->
+  
+
+</body>
+
